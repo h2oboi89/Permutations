f1d7ca1e29a69ef3bb3924ea0c3ccda659b4fa28
h2oboi89 <h2oboi89@gmail.com>
2015-04-02 22:38:22 -0400

    generate -> permute, made generate private

--- heap.py
+++ heap.py
@@ -1,4 +1,4 @@
-def generate(values, count = -1):
+def _generate(values, count = -1):
     if count == -1:
         count = len(values)
 
@@ -6,7 +6,7 @@ def generate(values, count = -1):
         yield values
     else:
         for i in range(count):
-            for permutation in generate(values, count - 1):
+            for permutation in _generate(values, count - 1):
                 yield permutation
 
             if count % 2 == 1:
@@ -15,3 +15,20 @@ def generate(values, count = -1):
                 j = i
 
             values[j], values[count - 1] = values[count - 1], values[j]
+
+def permute(value):
+    values = []
+    if type(value) is str:
+        for c in value:
+            values.append(c)
+
+        return ''.join(_generate(values))
+
+    elif type(value) is int:
+        for c in str(value):
+            values.append(c)
+
+        return int(''.join(_generate(values)))
+
+    elif type(value) is list:
+        return _generate(value)
