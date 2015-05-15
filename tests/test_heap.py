391a882cce3296c314cb15f9f7a16b17b8b65eb1
h2oboi89 <h2oboi89@gmail.com>
2015-05-14 23:19:41 -0400

    fixed unit test

--- tests/test_heap.py
+++ tests/test_heap.py
@@ -4,16 +4,16 @@ import sys, os, math
 
 sys.path.insert(0, os.path.pardir)
 
-import heap
+import permute
 
-class HeapTestCase(unittest.TestCase):
-    def test_heap(self):
+class PermuteTestCase(unittest.TestCase):
+    def test_perute(self):
         vals = []
 
         for i in range(5):
             vals.append(i)
 
-            self.assertEqual(len(list(heap.generate(vals))), math.factorial(len(vals)))
+            self.assertEqual(len(list(permute.permute(vals))), math.factorial(len(vals)))
 
 if __name__ == '__main__':
     unittest.main()
